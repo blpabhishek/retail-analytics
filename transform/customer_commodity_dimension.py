b872e577b8336a140cfec5f8b663e4501cce5be7
vikash <ksvikash@thoughtworks.com>
2017-02-09 12:39:33 +0300

    Getting commodities only for high sales department.

--- transform/customer_commodity_dimension.py
+++ transform/customer_commodity_dimension.py
@@ -18,23 +18,11 @@ invalidDepartment = ['CHARITABLE CONT', 'CNTRL/STORE SUP', 'DELI/SNACK BAR', 'EL
                      'SALES', 'RX', 'TOYS', 'VIDEO', 'VIDEO RENTAL', 'AUTOMOTIVE', 'DAIRY DELI', 'GM MERCH EXP',
                      'PROD-WHS SALES']
 
+validDepartment = ['DRUG GM', 'GROCERY', 'KIOSK-GAS', 'MEAT', 'MEAT-PCKGD', 'PRODUCE']
+
 productFrame = productFrame[-productFrame['PRODUCT_ID'].isin(invalidProducts)]
-productFrame = productFrame[-productFrame['DEPARTMENT'].isin(invalidDepartment)]
+productFrame = productFrame[productFrame['DEPARTMENT'].isin(validDepartment)]
 
-tdf1 = transactionFrame[['household_key', 'PRODUCT_ID', 'QUANTITY', 'SALES_VALUE']]
-pdf = productFrame[['PRODUCT_ID', 'COMMODITY_DESC']]
-pdf.columns = ['PRODUCT_ID', 'COMMODITY']
-
-commodity_purchase_behaviour = pd.merge(tdf1, pdf, on='PRODUCT_ID', how='left').groupby(
-    ['household_key', 'COMMODITY']).agg({"SALES_VALUE": 'sum', "QUANTITY": 'sum'}).unstack()
-commodity_purchase_behaviour = commodity_purchase_behaviour.reset_index().fillna(0)
-commodity_purchase_behaviour = commodity_purchase_behaviour.set_index('household_key')
-commodity_purchase_behaviour.head(10)
-
-commodity_purchase_behaviour['QUANTITY', 'BABY FOODS'] = commodity_purchase_behaviour['QUANTITY', 'BABY FOODS'] + \
-                                                         commodity_purchase_behaviour['QUANTITY', 'BABYFOOD']
-commodity_purchase_behaviour['SALES_VALUE', 'BABY FOODS'] = commodity_purchase_behaviour['SALES_VALUE', 'BABY FOODS'] + \
-                                                            commodity_purchase_behaviour['SALES_VALUE', 'BABYFOOD']
 tdf1 = transactionFrame[['household_key', 'PRODUCT_ID', 'SALES_VALUE']]
 pdf = productFrame[['PRODUCT_ID', 'COMMODITY_DESC']]
 pdf.columns = ['PRODUCT_ID', 'COMMODITY']
@@ -45,10 +33,14 @@ commodity_purchase_behaviour = pd.merge(tdf1, pdf, on='PRODUCT_ID', how='left')[
 commodity_purchase_behaviour = commodity_purchase_behaviour.reset_index().fillna(0)
 commodity_purchase_behaviour = commodity_purchase_behaviour.set_index('household_key')
 commodity_purchase_behaviour = commodity_purchase_behaviour['SALES_VALUE']
-commodity_purchase_behaviour['BABY FOODS'] = commodity_purchase_behaviour['BABY FOODS'] + commodity_purchase_behaviour[
-    'BABYFOOD']
+commodity_purchase_behaviour['Total Sale'] = commodity_purchase_behaviour.sum(axis=1)
+commodity_purchase_behaviour = commodity_purchase_behaviour.div(commodity_purchase_behaviour['Total Sale'], axis=0)
+commodity_purchase_behaviour.drop('Total Sale', axis=1, inplace=True)
+# commodity_purchase_behaviour['BABY FOODS'] = commodity_purchase_behaviour['BABY FOODS']  + commodity_purchase_behaviour['BABYFOOD']
 commodity_purchase_behaviour.rename(columns={'(CORP USE ONLY)': 'CORP USE ONLY'}, inplace=True)
 commodity_purchase_behaviour.rename(columns=lambda x: "Commodity_" + x, inplace=True)
 commodity_purchase_behaviour.reset_index(inplace=True)
 
+commodity_purchase_behaviour.head(10)
 
+print commodity_purchase_behaviour.head(10)
