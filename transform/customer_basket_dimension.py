5b785f37aa65af7959eb360dd3f9b34c9531f903
vikash <ksvikash@thoughtworks.com>
2017-02-09 17:18:32 +0300

    Rename bucket to basket

--- transform/customer_basket_dimension.py
+++ transform/customer_basket_dimension.py
@@ -18,11 +18,11 @@ transactionFrame = transactionFrame[-transactionFrame['PRODUCT_ID'].isin(invalid
 
 tdf1 = transactionFrame[['household_key','BASKET_ID','PRODUCT_ID']]
 
-bucket_behavior = tdf1.groupby(['household_key','BASKET_ID']).agg({"PRODUCT_ID":'count'})
-bucket_behavior.columns = ['Item_Count']
-bucket_behavior.reset_index(inplace=True)
+basket_behavior = tdf1.groupby(['household_key','BASKET_ID']).agg({"PRODUCT_ID":'count'})
+basket_behavior.columns = ['Item_Count']
+basket_behavior.reset_index(inplace=True)
 
-bucket_behavior = bucket_behavior.groupby(['household_key']).agg({"BASKET_ID":'count',"Item_Count":'sum'}).reset_index()
-bucket_behavior.rename(columns={'BASKET_ID':'Basket_Count'},inplace=True)
-bucket_behavior['Avg_Basket_Items'] = bucket_behavior['Item_Count'] / bucket_behavior['Basket_Count']
-bucket_behavior.head(10)
+basket_behavior = basket_behavior.groupby(['household_key']).agg({"BASKET_ID":'count',"Item_Count":'sum'}).reset_index()
+basket_behavior.rename(columns={'BASKET_ID':'Basket_Count'},inplace=True)
+basket_behavior['Avg_Basket_Items'] = basket_behavior['Item_Count'] / basket_behavior['Basket_Count']
+basket_behavior.head(10)
