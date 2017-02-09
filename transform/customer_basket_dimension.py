d4f890009f754debbee6bafab9b106f4d8bf225e
vikash <ksvikash@thoughtworks.com>
2017-02-09 11:55:26 +0300

    Evaluate the average of items purchased in basket by customer.

--- transform/customer_basket_dimension.py
+++ transform/customer_basket_dimension.py
@@ -11,13 +11,18 @@ transactionFrame = pd.read_csv('transaction_data.csv')
 os.chdir(path)
 
 
-# transactionFrame.columns
-tdf1 = transactionFrame[['household_key', 'BASKET_ID']]
-basket_behavior = tdf1.groupby(['household_key']).agg({"BASKET_ID": 'count'}).reset_index()
-basket_behavior.columns = ['household_key', 'Basket Count']
-basket_behavior.head(10)
+invalidProducts = [5126106, 5993055, 5978657, 5126087, 5993051, 5978650, 5978659, 6693056, 5993054, 5126088, 5126107,
+                   5978649, 5977100, 5978656, 5978648]
 
-# cluster_fatures = ['Sale Value','Discount Percentage']
-# df_cluster = customer_discount_behaviour[cluster_fatures]
-# percentileList = [.01, .02, .03, .05, .10, .20, .25, .30, .40, .50, .60, .70, .75, .80, .90, .95, .97, .98, .99]
-# df_cluster.describe(percentiles=percentileList).head(0)
+transactionFrame = transactionFrame[-transactionFrame['PRODUCT_ID'].isin(invalidProducts)]
+
+tdf1 = transactionFrame[['household_key','BASKET_ID','PRODUCT_ID']]
+
+bucket_behavior = tdf1.groupby(['household_key','BASKET_ID']).agg({"PRODUCT_ID":'count'})
+bucket_behavior.columns = ['Item_Count']
+bucket_behavior.reset_index(inplace=True)
+
+bucket_behavior = bucket_behavior.groupby(['household_key']).agg({"BASKET_ID":'count',"Item_Count":'sum'}).reset_index()
+bucket_behavior.rename(columns={'BASKET_ID':'Basket_Count'},inplace=True)
+bucket_behavior['Avg_Basket_Items'] = bucket_behavior['Item_Count'] / bucket_behavior['Basket_Count']
+bucket_behavior.head(10)
