d093054509a02fbdc9e6010c3429bedeb9c5393c
Abhishek Gupta <abhishkg@thoughtworks.com>
2017-02-16 10:08:55 +0530

    Refactoring..

--- transform/customer_brand_dimension.py
+++ transform/customer_brand_dimension.py
@@ -6,20 +6,18 @@ path = os.environ['data_path']
 os.chdir(path + "/source")
 
 productFrame = pd.read_csv('product.csv')
-customerFrame = pd.read_csv('hh_demographic.csv')
 transactionFrame = pd.read_csv('transaction_data.csv')
 os.chdir(path)
 
-tdf1 = transactionFrame[['household_key', 'PRODUCT_ID', 'SALES_VALUE']]
-tdf1.columns = ['household_key', 'PRODUCT Count', 'Sales']
-pdf = productFrame[['PRODUCT_ID', 'BRAND']]
-pdf.columns = ['PRODUCT Count', 'BRAND']
+product_and_sales = transactionFrame[['household_key', 'PRODUCT_ID', 'SALES_VALUE']]
+product_and_sales.columns = ['household_key', 'PRODUCT Count', 'Sales']
+product_brand = productFrame[['PRODUCT_ID', 'BRAND']]
+product_brand.columns = ['PRODUCT Count', 'BRAND']
 
-brand_purchase_behaviour = pd.merge(tdf1, pdf, on='PRODUCT Count') \
-    .groupby(['household_key', 'BRAND']) \
-    .agg({"PRODUCT Count": 'count', "Sales": 'sum'}).unstack()
-brand_purchase_behaviour = brand_purchase_behaviour.reset_index()
-brand_purchase_behaviour = brand_purchase_behaviour[['PRODUCT Count', 'Sales', 'household_key']]
+product_sales_brand = pd.merge(product_and_sales, product_brand, on='PRODUCT Count').groupby(
+    ['household_key', 'BRAND']).agg({"PRODUCT Count": 'count', "Sales": 'sum'}).unstack().reset_index()
+
+brand_purchase_behaviour = product_sales_brand[['PRODUCT Count', 'Sales', 'household_key']]
 brand_purchase_behaviour['Total Product Count'] = brand_purchase_behaviour['PRODUCT Count', 'National'] + \
                                                   brand_purchase_behaviour['PRODUCT Count', 'Private']
 brand_purchase_behaviour['Total Product Sales'] = brand_purchase_behaviour['Sales', 'National'] + \
--- transform/customer_department_dimension.py
+++ transform/customer_department_dimension.py
@@ -8,32 +8,26 @@ path = os.environ['data_path']
 os.chdir(path + "/source")
 
 productFrame = pd.read_csv('product.csv')
-customerFrame = pd.read_csv('hh_demographic.csv')
 transactionFrame = pd.read_csv('transaction_data.csv')
+
 os.chdir(path)
 
-productFrame = productFrame[-productFrame['PRODUCT_ID'].isin(invalidProducts)]
-productFrame = productFrame[productFrame['DEPARTMENT'].isin(validDepartment)]
-tdf1 = transactionFrame[['household_key', 'PRODUCT_ID', 'SALES_VALUE']]
-pdf = productFrame[['PRODUCT_ID', 'DEPARTMENT']]
+valid_products = productFrame[-productFrame['PRODUCT_ID'].isin(invalidProducts)]
+valid_products_in_valid_dept = valid_products[valid_products['DEPARTMENT'].isin(validDepartment)]
 
-department_purchase_behaviour = pd.merge(tdf1, pdf, on='PRODUCT_ID', how='left')[
-    ['household_key', 'DEPARTMENT', 'SALES_VALUE']].groupby(['household_key', 'DEPARTMENT']).sum().unstack()
-department_purchase_behaviour = department_purchase_behaviour.reset_index().fillna(0)
+product_sales = transactionFrame[['household_key', 'PRODUCT_ID', 'SALES_VALUE']]
+product_department = valid_products_in_valid_dept[['PRODUCT_ID', 'DEPARTMENT']]
+
+department_purchase_behaviour = pd.merge(product_sales, product_department, on='PRODUCT_ID', how='left')[
+    ['household_key', 'DEPARTMENT', 'SALES_VALUE']].groupby(
+    ['household_key', 'DEPARTMENT']).sum().unstack().reset_index().fillna(0)
+
 department_purchase_behaviour['SALES_VALUE', 'Total Sale'] = department_purchase_behaviour['SALES_VALUE'].sum(axis=1)
-department_purchase_behaviour.set_index('household_key', inplace=True)
-department_purchase_behaviour = department_purchase_behaviour['SALES_VALUE']
+department_purchase_behaviour = department_purchase_behaviour.set_index('household_key')['SALES_VALUE']
 department_purchase_behaviour.rename(columns=lambda x: "Department_" + x, inplace=True)
 department_purchase_behaviour = department_purchase_behaviour.div(
     department_purchase_behaviour['Department_Total Sale'], axis=0)
-department_purchase_behaviour.reset_index(inplace=True)
+department_purchase_behaviour = department_purchase_behaviour.reset_index()
 department_purchase_behaviour.drop('Department_Total Sale', axis=1, inplace=True)
 
-department_purchase_behaviour.head(10)
-
-# features_to_use = department_purchase_behaviour.columns
-# df_cluster = customer_department_purchase_behaviour[features_to_use]
-# df_cluster.drop([df_cluster.columns[0]], axis=1, inplace=True)
-
-# percentileList = [.01, .02, .03, .05, .10, .20, .25, .30, .40, .50, .60, .70, .75, .80, .90, .95, .97, .98, .99]
-# df_cluster.describe(percentiles=percentileList).transpose().to_csv('out1.csv')
+print department_purchase_behaviour.head(10)
